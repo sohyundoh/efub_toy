5e17ea98364aad1807fa076026830a6d5ddb57de
Eunbi Choi <pisr25@naver.com>
2022-05-22 23:35:55 +0900

    은비/styleOption 색상 추가
    styleOption 색상 추가

--- src/styles/styleOption.js
+++ src/styles/styleOption.js
@@ -3,4 +3,16 @@ export const colors = {
     1: "#1D9BF0",
     2: "#1D9BF01A",
   },
+
+  white: {
+    1: "#FFFFFF",
+  },
+
+  black: {
+    1: "#0F1419",
+  },
+
+  gray: {
+    1: "#A5AFB5",
+  },
 };
